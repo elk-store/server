7e08e568c02543106e2b2f6e89fd0f4b3fe1a2ee
Rafael Stein Matsumoto <rafaelmatsumoto43@gmail.com>
2020-05-26 19:54:06 -0300

    feat(product): adds entity

---
+++ src/modules/product/product.entity.ts
@@ -0,0 +1,43 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  BaseEntity,
+} from 'typeorm';
+
+import { Size } from './product.interface';
+
+@Entity('products')
+export class Product extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  public id: string;
+
+  @Column()
+  public name: string;
+
+  @Column()
+  public price: number;
+
+  @Column()
+  public quantity: number;
+
+  @Column()
+  public description: string;
+
+  @Column('simple-array')
+  public tags: string[];
+
+  @Column('simple-array')
+  public pictures: string[];
+
+  @Column('enum', { enum: Size })
+  public size: Size;
+
+  @CreateDateColumn()
+  public createdAt: Date;
+
+  @UpdateDateColumn()
+  public updatedAt: Date;
+}
