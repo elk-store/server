a7cd17a89846558704ec352644f12cf4f2c87eb8
Rafael Stein Matsumoto <rafaelmatsumoto43@gmail.com>
2020-05-26 20:09:52 -0300

    feat(product): add product module to api module

--- src/modules/api.module.ts
+++ src/modules/api.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AddressModule } from './address/address.module';
 import { PaymentModule } from './payment/payment.module';
+import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AddressModule, PaymentModule, UserModule],
+  imports: [AddressModule, PaymentModule, UserModule, ProductModule],
 })
 export class ApiModule {}
