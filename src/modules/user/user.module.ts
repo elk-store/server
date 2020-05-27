c8cb6e19cc5ba73dc82d5b09258399c18b4fd9b9
Henrique de Castilhos <henrique@castilhos.dev>
2020-05-26 21:31:14 -0300

    chore(user): updates module

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { User } from './user.entity';
+import { AuthModule } from '../../core/auth/auth.module';
+import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([UserRepository]), AuthModule],
+  controllers: [UserController],
+  providers: [UserService],
+  exports: [UserService],
 })
 export class UserModule {}
