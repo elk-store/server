5b2c20ad0d35f7db704d154576d64123bae706b3
Rafael Stein Matsumoto <rafaelmatsumoto43@gmail.com>
2020-06-08 19:28:24 -0300

    refactor: enhance parameters used in product factory

--- src/modules/product/specs/product.spec-helper.ts
+++ src/modules/product/specs/product.spec-helper.ts
@@ -1,13 +1,17 @@
 import { makeFactory } from 'factory.ts';
 
 import { ProductDTO } from '../product.dto';
+import { Size } from '../product.interface';
 
 export const ProductBuilder = makeFactory<ProductDTO>({
-  name: 'lasdfjaksj',
-  price: 22,
+  name: 'Laptop Backpack',
+  price: 25.75,
   quantity: 23,
-  description: 'lsafjkjfj',
-  tags: new Set(['lasdfk', 'ladkjsf']),
-  pictures: new Set(['lasdfk', 'ladkjsf']),
-  size: new Set([1, 2, 3]),
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+  tags: new Set(['Black', 'Summer']),
+  pictures: new Set([
+    'https://via.placeholder.com/300',
+    'https://via.placeholder.com/350',
+  ]),
+  size: new Set([Size.XS, Size.S, Size.M]),
 });
