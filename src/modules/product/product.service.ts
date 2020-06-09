2621531c869df888b39687f0539bd459c7785a79
Rafael Stein Matsumoto <rafaelmatsumoto43@gmail.com>
2020-06-09 20:09:17 -0300

    refactor: refactor product repository call in services

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,41 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { plainToClass } from 'class-transformer';
+import { Repository, DeleteResult } from 'typeorm';
 
 import { ProductDTO } from './product.dto';
 import { Product } from './product.entity';
-import { ProductRepository } from './product.repository';
 
 @Injectable()
 export class ProductService {
   constructor(
-    @InjectRepository(ProductRepository)
-    private productRepository: ProductRepository
+    @InjectRepository(Product)
+    private productRepository: Repository<Product>
   ) {}
 
-  public async findAll(): Promise<Product[]> {
-    return await this.productRepository.findAll();
+  public findAll(): Promise<Product[]> {
+    return this.productRepository.find();
   }
 
-  public async findById(id: string): Promise<Product> {
-    return await this.productRepository.findById(id);
+  public findById(id: string): Promise<Product> {
+    return this.productRepository.findOneOrFail(id);
   }
 
-  public async create(productRequest: ProductDTO): Promise<Product> {
+  public create(productRequest: ProductDTO): Promise<Product> {
     const product = plainToClass(Product, productRequest);
-    return await this.productRepository.persist(product);
+    return this.productRepository.save(product);
   }
 
-  public async update(
-    productRequest: ProductDTO,
-    id: string
-  ): Promise<Product> {
+  public update(productRequest: ProductDTO, id: string): Promise<Product> {
     const product = plainToClass(Product, productRequest);
     product.id = id;
-    return await this.productRepository.persist(product);
+    return this.productRepository.save(product);
   }
 
-  public async delete(id: string): Promise<string> {
-    return await this.productRepository.deleteById(id);
+  public delete(id: string): Promise<DeleteResult> {
+    return this.productRepository.delete(id);
   }
 }
