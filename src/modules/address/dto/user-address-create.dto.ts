2b53bb92e35d179e37e45e4b07c00e268f55cebc
Joao Seidel <joaovseidel@gmail.com>
2020-06-09 21:37:10 -0300

    feat: add swagger documentation in user-address-create.dto.ts

--- src/modules/address/dto/user-address-create.dto.ts
+++ src/modules/address/dto/user-address-create.dto.ts
@@ -1,28 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNumber, IsEmail } from 'class-validator';
 
 export class UserAddressCreateDTO {
+  @ApiProperty({
+    description: 'The e-mail of the associated user',
+    required: true,
+  })
   @IsString()
   @IsEmail()
   public userEmail: string;
 
+  @ApiProperty({ description: "The name of the address" })
   @IsString()
   public name: string;
 
+  @ApiProperty({ description: "The street name" })
   @IsString()
   public street: string;
 
+  @ApiProperty({ description: "The street number" })
   @IsNumber()
   public number: number;
 
+  @ApiProperty({ description: "The district of the address" })
   @IsString()
   public district: string;
 
+  @ApiProperty({ description: "The CEP"})
   @IsNumber()
   public cep: number;
 
+  @ApiProperty({ description: "The city" })
   @IsString()
   public city: string;
 
+  @ApiProperty({ description: "The state" })
   @IsString()
   public state: string;
 }
