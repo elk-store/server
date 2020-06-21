6e28d41cc187bff7f5e3278343f0ac8b62b0b149
Joao Seidel <joaovseidel@gmail.com>
2020-06-21 01:13:34 -0300

    feat(products-rest): add search dto

---
+++ src/modules/product/dtos/product-search.dto.ts
@@ -0,0 +1,9 @@
+export class ProductSearchDTO {
+  public id: string;
+  public name: string;
+  public minPrice: number;
+  public maxPrice: number;
+  public tags: string[];
+  public sizes: string[];
+  public orders: string[];
+}
