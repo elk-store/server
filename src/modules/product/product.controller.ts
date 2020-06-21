652917544fe8ac2f1f2cd2e6b02af8767e88cfdc
Joao Seidel <joaovseidel@gmail.com>
2020-06-21 01:16:18 -0300

    feat(products-rest): add filter query params to controller

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -8,6 +8,7 @@ import {
   Body,
   Put,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import {
@@ -17,14 +18,19 @@ import {
   ApiParam,
   ApiResponse,
   ApiTags,
+  ApiQuery,
 } from '@nestjs/swagger';
 import { plainToClass } from 'class-transformer';
+import { isEmpty } from 'class-validator';
+import { Pagination } from 'nestjs-typeorm-paginate';
 import { DeleteResult } from 'typeorm';
 
 import { RequiredRoles } from '../../core/auth/required-roles.decorator';
 import { UserRole } from '../user/user.interface';
 import { ProductCreateDTO } from './dtos/product-create.dto';
 import { ProductResponseDTO } from './dtos/product-response.dto';
+import { ProductSearchDTO } from './dtos/product-search.dto';
+import { Product } from './product.entity';
 import { ProductService } from './product.service';
 
 @ApiTags('Products')
@@ -37,12 +43,76 @@ export class ProductController {
     status: 200,
     type: ProductResponseDTO,
   })
+  @ApiQuery({
+    name: 'name',
+    description: 'Filter by product name',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'max-price',
+    description: 'Filter by a max price',
+    required: false,
+  })
+  @ApiQuery({
+    name: 'min-price',
+    description: 'Filter by a min price',
+    required: false,
+  })
+  @ApiQuery({ name: 'tags', description: 'Filter by tags', required: false })
+  @ApiQuery({ name: 'sizes', description: 'Filter by sizes', required: false })
+  @ApiQuery({ name: 'order', description: 'Ordination', required: false })
+  @ApiQuery({ name: 'page', description: 'Page start number', required: false })
+  @ApiQuery({
+    name: 'limit',
+    description: 'Page limit number',
+    required: false,
+  })
   @Get()
-  public async findAll(): Promise<ProductResponseDTO[]> {
-    const allProducts = await this.productService.findAll();
-    return allProducts.map(product => {
-      return plainToClass(ProductResponseDTO, product);
+  public async findAll(
+    @Query('name') name: string,
+    @Query('max-price') maxPrice = null,
+    @Query('min-price') minPrice = null,
+    @Query('tags') tags: string = null,
+    @Query('sizes') sizes: string = null,
+    @Query('order') order: string = null,
+    @Query('page') page = 1,
+    @Query('limit') limit = 10
+  ): Promise<Pagination<ProductResponseDTO>> {
+    const searchDto = new ProductSearchDTO();
+    searchDto.name = name;
+    searchDto.maxPrice = maxPrice;
+    searchDto.minPrice = minPrice;
+
+    if (!isEmpty(sizes)) {
+      sizes = sizes.replace(' ', '');
+      searchDto.sizes = sizes.split(',');
+    }
+
+    if (!isEmpty(tags)) {
+      tags = tags.replace(' ', '');
+      searchDto.tags = tags.split(',');
+    }
+
+    if (!isEmpty(order)) {
+      order = order.replace(' ', '');
+      searchDto.orders = order.split(',');
+    }
+
+    const results = await this.productService.paginate(searchDto, {
+      page,
+      limit,
+      route: '/products',
     });
+
+    return new Pagination(
+      await Promise.all(
+        results.items.map(async (item: Product) => {
+          return plainToClass(ProductResponseDTO, item);
+        })
+      ),
+      results.meta,
+      results.links
+    );
   }
 
   @ApiOperation({ summary: 'List information of a specific product' })
@@ -55,7 +125,10 @@ export class ProductController {
   public async findById(
     @Param('id', ParseUUIDPipe) id: string
   ): Promise<ProductResponseDTO> {
-    const product = this.productService.findById(id);
+    const productSearch = new ProductSearchDTO();
+    productSearch.id = id;
+
+    const product = await this.productService.find(productSearch);
     return plainToClass(ProductResponseDTO, product);
   }
 
