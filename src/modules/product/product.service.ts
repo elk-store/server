88ec647d152bfd95de02fa9693b81dbc4929a852
Joao Seidel <joaovseidel@gmail.com>
2020-06-21 01:15:53 -0300

    feat(products-rest): add querybuilder and filter options

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { plainToClass } from 'class-transformer';
-import { Repository, DeleteResult } from 'typeorm';
+import {
+  IPaginationOptions,
+  Pagination,
+  paginate,
+} from 'nestjs-typeorm-paginate';
+import { Repository, DeleteResult, SelectQueryBuilder } from 'typeorm';
 
 import { ProductCreateDTO } from './dtos/product-create.dto';
+import { ProductSearchDTO } from './dtos/product-search.dto';
 import { Product } from './product.entity';
 
 @Injectable()
@@ -13,20 +19,15 @@ export class ProductService {
     private productRepository: Repository<Product>
   ) {}
 
-  public findAll(): Promise<Product[]> {
-    return this.productRepository.find();
-  }
-
-  public findById(id: string): Promise<Product> {
-    return this.productRepository.findOneOrFail(id);
-  }
-
   public create(productRequest: ProductCreateDTO): Promise<Product> {
     const product = plainToClass(Product, productRequest);
     return this.productRepository.save(product);
   }
 
-  public update(productRequest: ProductCreateDTO, id: string): Promise<Product> {
+  public update(
+    productRequest: ProductCreateDTO,
+    id: string
+  ): Promise<Product> {
     const product = plainToClass(Product, productRequest);
     product.id = id;
     return this.productRepository.save(product);
@@ -34,5 +35,90 @@ export class ProductService {
 
   public delete(id: string): Promise<DeleteResult> {
     return this.productRepository.delete(id);
+  }
+
+  public find(productSearch: ProductSearchDTO): Promise<Product> {
+    return this.queryBuilder(productSearch).getOne();
+  }
+
+  public paginate(
+    productSearch: ProductSearchDTO,
+    options: IPaginationOptions
+  ): Promise<Pagination<Product>> {
+    return paginate<Product>(this.queryBuilder(productSearch), options);
+  }
+
+  private queryBuilder(
+    productSearch: ProductSearchDTO
+  ): SelectQueryBuilder<Product> {
+    const queryBuilder = this.productRepository.createQueryBuilder('product');
+    queryBuilder.where('product.id IS NOT NULL');
+
+    if (productSearch.id !== null && productSearch.id !== undefined) {
+      queryBuilder.andWhere('product.id = :id', {
+        id: productSearch.id,
+      });
+    }
+
+    if (productSearch.name !== null && productSearch.name !== undefined) {
+      queryBuilder.andWhere('product.name ilike :name', {
+        name: '%' + productSearch.name + '%',
+      });
+    }
+
+    if (
+      productSearch.minPrice !== null &&
+      productSearch.minPrice !== undefined
+    ) {
+      queryBuilder.andWhere('product.price >= :minPrice', {
+        minPrice: productSearch.minPrice,
+      });
+    }
+
+    if (
+      productSearch.maxPrice !== null &&
+      productSearch.maxPrice !== undefined
+    ) {
+      queryBuilder.andWhere('product.price <= :maxPrice', {
+        maxPrice: productSearch.maxPrice,
+      });
+    }
+
+    if (productSearch.tags !== null && productSearch.tags !== undefined) {
+      const searchTags = productSearch.tags
+        .map(tag => {
+          return '%' + tag + '%';
+        })
+        .join(',');
+
+      queryBuilder.andWhere('product.tags ilike :tags', {
+        tags: searchTags,
+      });
+    }
+
+    if (productSearch.sizes !== null && productSearch.sizes !== undefined) {
+      const searchSizes = productSearch.sizes
+        .map(size => {
+          return '%' + size + '%';
+        })
+        .join(',');
+
+      queryBuilder.andWhere('product.sizes ilike :sizes', {
+        sizes: searchSizes,
+      });
+    }
+
+    if (productSearch.orders !== null && productSearch.orders !== undefined) {
+      productSearch.orders.forEach(order => {
+        if (order.startsWith('-')) {
+          order = order.replace('-', '');
+          queryBuilder.addOrderBy('product.' + order, 'DESC');
+        } else {
+          queryBuilder.addOrderBy('product.' + order, 'ASC');
+        }
+      });
+    }
+
+    return queryBuilder;
   }
 }
