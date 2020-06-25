504a423e40952e2907a57446c92199dc64f17cbf
Joao Seidel <joaovseidel@gmail.com>
2020-06-25 17:27:22 -0300

    feat: add recovery payloads and recovery code cache

--- src/core/auth/auth.interface.ts
+++ src/core/auth/auth.interface.ts
@@ -1,6 +1,26 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
+export class PasswordRecoveryCache {
+  static list: Map<string, string> = new Map<string, string>();
+}
+
+export class RecoverySentPayload {
+  @ApiProperty({ description: 'The email for password recovering' })
+  @IsEmail()
+  email: string;
+}
+
+export class RecoveryPayload {
+  @ApiProperty({
+    description: 'The new password of the user',
+    minimum: 8,
+  })
+  @MinLength(8)
+  @IsString()
+  password: string;
+}
+
 export class LoginPayload {
   @ApiProperty({ description: 'The email of an user' })
   @IsEmail()
